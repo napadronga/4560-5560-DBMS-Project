6fcc0793b146287fb452727182efdc234ba2852f
tannersloan05 <tannersloan05@gmail.com>
2025-11-27 11:58:24 -0600

    Update view_records.php

--- patient/view_records.php
+++ patient/view_records.php
@@ -35,7 +35,7 @@ $medications = $medications_result->fetch_all(MYSQLI_ASSOC);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient Health Records</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/styles.css?v=5">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -108,3 +108,4 @@ $medications = $medications_result->fetch_all(MYSQLI_ASSOC);
     </div>
 </body>
 </html>
+
